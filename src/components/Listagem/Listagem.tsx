7bfde075b954d9c92bab1091a70a6db7715558bd
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-05 14:28:41 -0300

    Adicionei responsividade na listagem, mudando a orientação da tablea em dispositivos móveis

--- src/components/Listagem/Listagem.tsx
+++ src/components/Listagem/Listagem.tsx
@@ -7,7 +7,7 @@ export default function Listagem() {
       <thead>
         <tr>
           <th>Moeda</th>
-          <th>Valor no Mercado</th>
+          <th>Valor de Mercado</th>
           <th>Preço</th>
           <th>Volume</th>
           <th>24h</th>
@@ -16,11 +16,19 @@ export default function Listagem() {
 
       <tbody>
         <tr>
-          <th>Bitcoin</th>
-          <th>Valor no Mercado</th>
-          <th>R$ 50,00</th>
-          <th>300</th>
-          <th>-200</th>
+          <td data-label="Moeda">Bitcoin</td>
+          <td data-label="Valor de Mercado">Valor no Mercado</td>
+          <td data-label="Preço">R$ 50,00</td>
+          <td data-label="Volume">300</td>
+          <td data-label="24h">-200</td>
+        </tr>
+
+        <tr>
+          <td data-label="Moeda">Bitcoin</td>
+          <td data-label="Valor de Mercado">Valor no Mercado</td>
+          <td data-label="Preço">R$ 50,00</td>
+          <td data-label="Volume">300</td>
+          <td data-label="24h">-200</td>
         </tr>
       </tbody>
 
