210cc005f0b8a687be72ab9f7fe91c2e0ec4060a
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-07 14:46:21 -0300

    Armazenando a pesquisa por nome em uma state

--- src/components/Pesquisa/Pesquisa.tsx
+++ src/components/Pesquisa/Pesquisa.tsx
@@ -1,12 +1,23 @@
+'use client'
+
+import { useState } from "react"
 import "./pesquisa.css"
 
 export default function Pesquisa() {
+  const [pesquisa, setPesquisa] = useState<string>("")
+
+  function pesquisar(e:React.FormEvent) {
+    e.preventDefault()
+    alert(pesquisa + " pesquisada")
+  }
+
   return (
-    <form>
+    <form onSubmit={pesquisar}>
       <input  
         type="text"
         placeholder="Digite o nome da criptomoeda"
         required
+        onChange={(e)=>setPesquisa(e.target.value)}
       />
       <button>
         <i className="fa-solid fa-magnifying-glass fa-lg" aria-hidden={true}></i>
