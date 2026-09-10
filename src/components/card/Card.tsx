c3b8058035518921509cd7980e7131c5b405bd94
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-09-10 11:22:35 -0300

    Preenchi as informações dinamicas no card e estilizei

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,11 +10,14 @@ export default function Card( { cripto, fecharCard }: cardProps ) {
   return (
     <div className="background">
       <div className="card">
-        <div className="infos">
+        <div className="header">
           <img src={`https://assets.coincap.io/assets/icons/${cripto.symbol.toLowerCase()}@2x.png`} />
           <h1>{cripto.name}</h1>
         </div>
-        <span></span>
+        <div className="infos">
+          <span><strong>Preço em Dólar: </strong>U$ {Number(cripto.priceUsd).toFixed(2)}</span>
+          <span><strong>Valor de Mercado: </strong>U$ {Number(cripto.marketCapUsd).toFixed(2)}</span>
+        </div>
         <button onClick={fecharCard}>Fechar</button>
       </div>
     </div>
